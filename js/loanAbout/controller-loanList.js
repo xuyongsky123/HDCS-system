9e26333403f69d8e7d44789173eff77c01f9af9f
snowwintersweet <2645627042@qq.com>
2017-07-12 10:53:26 +0800

    添加贷款业务模块

---
+++ js/loanAbout/controller-loanList.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Administrator on 2017/7/7.
+ */
+/*angular模块部分*/
+var app=angular.module('preContractDealApp',[]);
+app.controller("preContractDealCtrl",function($scope,$http){
+    $scope.tools={};
+    $scope.state={};
+    $scope.dictionary={
+        identity:identity,
+        bankData:bankData
+    };
+    $scope.dataModel={
+        userInfo:{
+            num:"",
+            phone:"",
+            identity:0,
+            earning:"",
+            identityNum:"",
+            fundNum:"",
+            workUnit:"",
+            contact:""
+        },
+        bankInfo:{
+            applyBank:"",
+            applyMoney:""
+        },
+        houseInfo:{
+            code:"2017房验第00002447号",
+            obligeeName:"程红",
+            identityType:0,
+            identityNum:"522424199108075008",
+            houseNum:"201708060003",
+            houseArea:"89.9",
+            insideArea:"86.9",
+            outsideArea:"2.8",
+            totalLayer:"17",
+            layer:"2",
+            houseLocation:"乌鲁木齐市水磨沟区"
+        }
+    };
+    $scope.ui={
+
+    };
+    /*实际付款数据模拟*/
+    $http.post('data/bankAddContract.json').then(function(data){
+        $scope.dataModel.readOnly=data.data;
+    },function(){
+
+    });
+});
---
+++ js/loanAbout/controller-loanRecheck.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Administrator on 2017/7/7.
+ */
+/*angular模块部分*/
+var app=angular.module('loanRecheckApp',[]);
+app.controller("loanRecheckCtrl",function($scope,$http){
+    $scope.tools={};
+    $scope.state={};
+    $scope.dictionary={
+        identity:identity,
+        bankData:bankData
+    };
+    $scope.dataModel={
+        proposerInfo:{
+            num:"",
+            phone:"",
+            identity:"",
+            earning:"",
+            identityNum:"",
+            fundNum:"",
+            workUnit:"",
+            contact:""
+        },
+        bankInfo:{
+            applyBank:"",
+            applyMoney:""
+        },
+        houseInfo:{
+            code:"2017房验第00002447号",
+            obligeeName:"程红",
+            identityType:"居民身份证",
+            identityNum:"522424199108075008",
+            houseNum:"201708060003",
+            houseArea:"89.9",
+            insideArea:"86.9",
+            outsideArea:"2.8",
+            totalLayer:"17",
+            layer:"2",
+            houseLocation:"乌鲁木齐市水磨沟区"
+        }
+    };
+    $scope.ui={
+
+    };
+    /*实际付款数据模拟*/
+    $http.post('data/bankAddContract.json').then(function(data){
+        $scope.dataModel.readOnly=data.data;
+    },function(){
+
+    });
+});
