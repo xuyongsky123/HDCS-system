1a074d0101dad98ce39d525493bd5da8cb200fd1
snowwintersweet <2645627042@qq.com>
2017-07-14 14:27:28 +0800

    修改登录页样式和数据字典更新

--- js/base-config/base-xm.js
+++ js/base-config/base-xm.js
@@ -11,22 +11,31 @@ var bankData=[
     {key:1,value:'上海银行'},
     {key:2,value:'招商银行'}
 ];
+
 var useMethods=[
-    {key:0,value:'规划用途1'},
-    {key:1,value:'规划用途2'},
-    {key:2,value:'规划用途3'}
+    {key:0,value:'住宅'},
+    {key:1,value:'非住宅'},
+    {key:2,value:'地下室'},
+    {key:3,value:'车库'}
 ];
+
 var houseCharacters=[
-    {key:0,value:"房屋性质1"},
-    {key:1,value:'房屋性质2'},
-    {key:2,value:'房屋性质3'},
-    {key:3,value:'房屋性质4'}
+    {key:0,value:"经济适用房"},
+    {key:1,value:'新建商品房'},
+    {key:2,value:'房改房'},
+    {key:3,value:'存量房产'},
+    {key:4,value:'拆迁安置'},
+    {key:5,value:'公寓'},
+    {key:5,value:'集资建房'}
 ];
 var houseMethods=[
     {key:0,value:"买卖"},
-    {key:1,value:'继承'},
-    {key:2,value:'裁决'},
-    {key:3,value:'赠与'}
+    {key:1,value:'赠与'},
+    {key:2,value:'置换'},
+    {key:3,value:'裁决'},
+    {key:4,value:'拍卖'},
+    {key:5,value:'继承'},
+    {key:6,value:"离婚分产"}
 ];
 var houseStructures=[
     {key:0,value:"住宅用房"},
@@ -39,11 +48,10 @@ var payNums=[
     {key:3,value:3},
     {key:4,value:4}
 ];
+
 var sellMethods=[
-    {key:0,value:"交割方式1"},
-    {key:1,value:'交割方式2'},
-    {key:2,value:'交割方式3'},
-    {key:3,value:'交割方式4'}
+    {key:0,value:"自行交割"},
+    {key:1,value:'委托资金监管机构监管'}
 ];
 var breakTimes=[
     {key:0,value:"1日内"},
@@ -51,11 +59,10 @@ var breakTimes=[
     {key:2,value:'14日内'},
     {key:3,value:'30日内'}
 ];
+
 var handleMethods=[
-    {key:0,value:"人民法院诉讼1"},
-    {key:1,value:'人民法院诉讼2'},
-    {key:2,value:'人民法院诉讼3'},
-    {key:3,value:'人民法院诉讼4'}
+    {key:0,value:"仲裁委员会仲裁"},
+    {key:1,value:'人民法院起诉'}
 ];
 var middleAgents=[
     {key:0,value:"中介1"},
